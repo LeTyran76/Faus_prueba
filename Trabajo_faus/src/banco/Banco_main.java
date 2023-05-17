6faffcae0ed0a7138087de4851ed32bd26660a17
Sergio <letyranscrip@gmail.com>
2023-05-17 18:24:03 +0200

    primer commit

---
+++ Trabajo_faus/src/banco/Banco_main.java
@@ -0,0 +1,10 @@
+package banco;
+
+public class Banco_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Trabajo_faus/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author letyr
+ *
+ */
+module Trabajo_faus {
+}
